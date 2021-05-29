b2088e391e1e779c7cf6d37b8909549fc7bd415d
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-30 00:48:51 +0530

    wrap the Container() widget along with all its contents inside of a new Column() widget as its child

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -5,52 +5,56 @@ class TasksScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.redAccent,
-      body: Container(
-        padding:
-            EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: <Widget>[
-            CircleAvatar(
-              backgroundColor: Colors.black,
-              radius: 30.0,
-              child: Icon(
-                Icons.list,
-                size: 40.0,
-                color: Colors.redAccent,
-              ),
-            ),
-            SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              'Todoer',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 50.0,
-                fontWeight: FontWeight.w700,
-              ),
-            ),
-            Text(
-              '12 Tasks',
-              style: TextStyle(
-                color: Colors.black,
-                fontSize: 18.0,
-              ),
-            ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.black87,
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(20.0),
-                    topRight: Radius.circular(20.0),
+      body: Column(
+        children: <Widget>[
+          Container(
+            padding: EdgeInsets.only(
+                top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundColor: Colors.black,
+                  radius: 30.0,
+                  child: Icon(
+                    Icons.list,
+                    size: 40.0,
+                    color: Colors.redAccent,
                   ),
                 ),
-              ),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text(
+                  'Todoer',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 50.0,
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+                Text(
+                  '12 Tasks',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  ),
+                ),
+                Expanded(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.black87,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(20.0),
+                        topRight: Radius.circular(20.0),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
