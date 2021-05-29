246f8c42071073eed3b3ecf3cfdb0c83d9d0cca7
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-29 23:49:12 +0530

    update the backgroundColor property for the Scaffold() widget

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -4,7 +4,7 @@ class TasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightBlueAccent,
+      backgroundColor: Colors.redAccent,
       body: Container(
         padding:
             EdgeInsets.only(top: 60.0, left: 30.0, right: 30.0, bottom: 30.0),
