f6c7068f4e04c99f612cef29127b5b8cbc74ee32
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-05-31 00:25:11 +0530

    paste the cut out part of the code inside the file task_tile.dart

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class TaskTile extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        'This is a task.',
+        style: TextStyle(
+            // color: Colors.white,
+            ),
+      ),
+      trailing: Checkbox(
+        value: false,
+        onChanged: null,
+      ),
+    );
+  }
+}
