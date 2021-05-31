7e274ae3cf4d9030ee68e72f447d7cb4ab1aa236
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 00:06:10 +0530

    add the borderRadius property for the BoxDecoration() widget

--- lib/screens/add_tasks_screen.dart
+++ lib/screens/add_tasks_screen.dart
@@ -7,8 +7,11 @@ class AddTasksScreen extends StatelessWidget {
       color: Color(0xff757575),
       child: Container(
         decoration: BoxDecoration(
-          color: Colors.white,
-        ),
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(20.0),
+              topLeft: Radius.circular(20.0),
+            )),
       ),
     );
   }
