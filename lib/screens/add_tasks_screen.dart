b01b57828c7028472794decba3845562fc8d136b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 00:05:08 +0530

    add the color property for the BoxDecoration() widget

--- lib/screens/add_tasks_screen.dart
+++ lib/screens/add_tasks_screen.dart
@@ -6,7 +6,9 @@ class AddTasksScreen extends StatelessWidget {
     return Container(
       color: Color(0xff757575),
       child: Container(
-        decoration: BoxDecoration(),
+        decoration: BoxDecoration(
+          color: Colors.white,
+        ),
       ),
     );
   }
