aaeabfafceae64a77e686558628a23910f830931
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 19:27:33 +0530

    add the crossAxisAlignment property for the Column() widget

--- lib/screens/add_tasks_screen.dart
+++ lib/screens/add_tasks_screen.dart
@@ -14,6 +14,7 @@ class AddTasksScreen extends StatelessWidget {
           ),
         ),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
             Text(
               'Add Task',
