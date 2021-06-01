cbf6a6cb33434a4e9a86fdec6c9faae3e655985b
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 23:42:34 +0530

    Comment out some more part of the code

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -32,15 +32,19 @@ class _TaskTileState extends State<TaskTile> {
   }
 }
 
-class TaskCheckbox extends StatelessWidget {
-  final bool checkboxState;
-  final void Function(bool?)
-      toggleCheckboxState; // Updated as per the latest version. Instead of initializing it as final Function, initialized it as void Function(bool?). Because the onChanged property of the Checkbox() widget accepts 'void Function(bool?)' type function.
-
-  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
-
-  @override
-  Widget build(BuildContext context) {
-    return;
-  }
-}
+// class TaskCheckbox extends StatelessWidget {
+//   final bool checkboxState;
+//   final void Function(bool?)
+//       toggleCheckboxState; // Updated as per the latest version. Instead of initializing it as final Function, initialized it as void Function(bool?). Because the onChanged property of the Checkbox() widget accepts 'void Function(bool?)' type function.
+//
+//   TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Checkbox(
+//       activeColor: Colors.redAccent,
+//       value: checkboxState,
+//       onChanged: toggleCheckboxState,
+//     );
+//   }
+// }
