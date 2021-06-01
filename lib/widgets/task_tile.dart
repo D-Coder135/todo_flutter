0f997ee7639bfbd196f093159bafce30d9acddb8
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-01 22:12:43 +0530

    wrap the initialization inside the constructor as named parameters

--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -32,7 +32,7 @@ class TaskCheckbox extends StatelessWidget {
   final bool checkboxState;
   final Function toggleCheckboxState;
 
-  TaskCheckbox(this.checkboxState, this.toggleCheckboxState);
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
 
   @override
   Widget build(BuildContext context) {
