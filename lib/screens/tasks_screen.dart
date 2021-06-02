2724c0f1ce3ae9851d6137a85b0478c892438fe0
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-02 23:21:26 +0530

    paste the cut out part inside the _TasksScreenState class inside the file tasks_screen.dart

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -8,6 +8,12 @@ class TasksScreen extends StatefulWidget {
 }
 
 class _TasksScreenState extends State<TasksScreen> {
+  List<Task> tasks = [
+    Task(name: 'Buy Milk'),
+    Task(name: 'Buy Eggs'),
+    Task(name: 'Buy Bread'),
+  ];
+
   Widget buildBottomSheet(BuildContext context) {
     return AddTasksScreen();
   }
