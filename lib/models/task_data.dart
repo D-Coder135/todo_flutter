c6bf9bda4a4ab04341b71352c8755d50c83a7612
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-03 23:52:52 +0530

    update the field list tasks and made it as private

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -2,7 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:todo_flutter/models/task.dart';
 
 class TaskData extends ChangeNotifier {
-  List<Task> tasks = [
+  List<Task> _tasks = [
     Task(name: 'Buy Milk'),
     Task(name: 'Buy Eggs'),
     Task(name: 'Buy Bread'),
