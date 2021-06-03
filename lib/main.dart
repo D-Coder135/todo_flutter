68502fa5c4ebc0992d8433fc6fb13d830363bd41
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-04 00:26:17 +0530

    add the create property for the widget ChangeNotifierProvider()

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:todo_flutter/models/task_data.dart';
 import 'package:todo_flutter/screens/tasks_screen.dart';
 
 void main() {
@@ -10,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
+      create: (BuildContext context) => TaskData(),
       child: MaterialApp(
         home: TasksScreen(),
       ),
