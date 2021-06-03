42214ae8f88bf8b84aaee7d3765183e3b3fc82cd
Devansh Shukla <59922056+D-Coder135@users.noreply.github.com>
2021-06-03 23:51:54 +0530

    moved the comment out part of the code inside the class TaskData and uncomment it

--- lib/models/task_data.dart
+++ lib/models/task_data.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/cupertino.dart';
+import 'package:todo_flutter/models/task.dart';
 
-class TaskData extends ChangeNotifier {}
-
-// List<Task> tasks = [
-//   Task(name: 'Buy Milk'),
-//   Task(name: 'Buy Eggs'),
-//   Task(name: 'Buy Bread'),
-// ];
+class TaskData extends ChangeNotifier {
+  List<Task> tasks = [
+    Task(name: 'Buy Milk'),
+    Task(name: 'Buy Eggs'),
+    Task(name: 'Buy Bread'),
+  ];
+}
